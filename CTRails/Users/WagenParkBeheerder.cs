d0f5c63c8e2ddab414d43d2f6cde1e668ae9c9c6
Nivade <nhavandeursen@gmail.com>
2016-04-07 09:40:45 +0200

    Create Users folder. Updated namespace names

---
+++ CTRails/Users/WagenParkBeheerder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace CTRails.Users
+{
+    class WagenParkBeheerder : User
+    {
+        public WagenParkBeheerder(string name, Geslacht gender, DateTime birthdate, string email, string nationality, string username, string password) : base(name, gender, birthdate, email, nationality, username, password) { }
+        public void GetTramLocatie(Tram tram) { }
+        public void BlockSection(Section section) { }
+    }
+}
