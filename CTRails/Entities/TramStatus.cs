be5018ce3f297e65531530df230edef26b160d92
EvelienHermans <emhermans98@gmail.com>
2016-04-13 14:26:07 +0200

    track sector, tramstatus, tram, route

---
+++ CTRails/Entities/TramStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTRails.Entities
+{
+    class TramStatus : Entity
+    {
+        //Fields
+        public Tram tram { get; set; }
+
+        public Status status { get; set; }
+
+
+        //Constructor
+        public TramStatus(int id, Tram tram, Status status) : base(id)
+        {
+            this.tram = tram;
+            this.status = status;
+        }
+    }
+}
