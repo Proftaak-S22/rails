dfd6f409b44fab805e149a1fc35a28873a522b22
Jelle Bouwmans <jelle.bouwmans@hotmail.nl>
2016-04-19 13:04:46 +0200

    GUI Popup 80%

---
+++ CTRails/PopUp1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace CTRails
+{
+    partial class PopUp1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddTram = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // btnAddTram
+            // 
+            this.btnAddTram.Depth = 0;
+            this.btnAddTram.Location = new System.Drawing.Point(59, 187);
+            this.btnAddTram.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnAddTram.Name = "btnAddTram";
+            this.btnAddTram.Primary = true;
+            this.btnAddTram.Size = new System.Drawing.Size(155, 56);
+            this.btnAddTram.TabIndex = 0;
+            this.btnAddTram.Text = "Plaats Tram";
+            this.btnAddTram.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(59, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 53);
+            this.textBox1.TabIndex = 1;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(55, 92);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(106, 19);
+            this.materialLabel1.TabIndex = 2;
+            this.materialLabel1.Text = "Tram Nummer";
+            // 
+            // PopUp1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 293);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAddTram);
+            this.Name = "PopUp1";
+            this.Text = "Tram plaatsen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton btnAddTram;
+        private System.Windows.Forms.TextBox textBox1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+    }
+}
