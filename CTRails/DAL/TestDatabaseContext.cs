e83a80fc4bf2eae377943499fe35e356e234853c
Nivade <nhavandeursen@gmail.com>
2016-04-21 13:51:59 +0200

    Add TestDatabaseContext

---
+++ CTRails/DAL/TestDatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CTRails.DAL.Contexts;
+using CTRails.Entities;
+using CTRails.Entities.Employees;
+
+
+namespace CTRails.DAL
+{
+    public abstract class TestDatabaseContext<T>
+    {
+        public List<T> Entities { get; protected set; } = new List<T>();
+
+    }
+}
