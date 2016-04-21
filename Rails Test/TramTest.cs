d2025495a2c8ec66e2a48e41909732a8b285b203
Bart Beekwilder <brtbeekwilder@gmail.com>
2016-04-21 13:14:12 +0200

    Unittests van Employee en Tram Tram kan nog geen meerdere statussen bevatten.

---
+++ Rails Test/TramTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CTRails;
+using System.Threading.Tasks;
+using CTRails.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rails_Test
+{
+    [TestClass]
+    public class TramTest
+    {
+        private UnitOfWork unitOfWork;
+        [TestInitialize]
+        public void Initialize()
+        {
+            unitOfWork = new UnitOfWork();
+        }
+
+        [TestMethod]
+        public void InsertTram()
+        {
+            
+            Tram tram = new Tram(1,
+                                 unitOfWork.Statuses.Where(status => status.Name()));
+            // Tram moet meerdere statussen kunnen bevatten.
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            unitOfWork = null;
+        }
+    }
+}
