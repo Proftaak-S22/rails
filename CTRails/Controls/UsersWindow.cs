a94fc260cb87862c1f61e3af779f1dcdf8842c16
Nivade <nhavandeursen@gmail.com>
2016-04-22 07:06:11 +0200

    Add users window

---
+++ CTRails/Controls/UsersWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CTRails.DAL;
+using CTRails.Entities.Employees;
+
+
+namespace CTRails.Controls
+{
+    public partial class UsersWindow : UserControl
+    {
+        public UsersWindow()
+        {
+            InitializeComponent();
+
+            LoadUsers();
+        }
+
+
+
+        private void LoadUsers()
+        {
+            UnitOfWork worker = new UnitOfWork(true);
+
+            foreach (Employee e in worker.Employees.Get())
+            {
+                ListViewItem item = new ListViewItem();
+                item.SubItems.Add(e.Username);
+                item.SubItems.Add(e.Gender.ToString());
+                item.SubItems.Add(e.DateOfBirth.ToShortDateString());
+                item.SubItems.Add(e.FirstName + " " + e.Prefix + " " + e.LastName);
+                item.SubItems.Add(e.Email);
+                item.SubItems.Add(e.Address.Country);
+                item.SubItems.Add(e.Address.City);
+                item.SubItems.Add(e.Address.Zipcode);
+                item.SubItems.Add(e.Address.Number.ToString());
+
+                lvGebruikers.Items.Add(item);
+            }
+        }
+    }
+}
